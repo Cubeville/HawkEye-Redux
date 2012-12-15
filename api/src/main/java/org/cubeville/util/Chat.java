91379e9a1f02894ecbdf19f3bd7d7a347ebf701c
GoalieGuy6 <goalieguy6@gmail.com>
2012-12-14 20:27:48 -0500

    Implemented color util

---
+++ api/src/main/java/org/cubeville/util/Chat.java
@@ -0,0 +1,97 @@
+package org.cubeville.util;
+
+public class Chat {
+
+	/**
+	 * Minecraft color escape sequence character
+	 */
+	public static final char FORMAT_CHAR = '\u00A7';
+
+	/**
+	 * Chat colors
+	 */
+	public static final String BLACK = FORMAT_CHAR + "0";
+	public static final String DARK_BLUE = FORMAT_CHAR + "1";
+	public static final String DARK_GREEN = FORMAT_CHAR + "2";
+	public static final String DARK_CYAN = FORMAT_CHAR + "3";
+	public static final String DARK_RED = FORMAT_CHAR + "4";
+	public static final String PURPLE = FORMAT_CHAR + "5";
+	public static final String GOLD = FORMAT_CHAR + "6";
+	public static final String GRAY = FORMAT_CHAR + "7";
+	public static final String DARK_GRAY = FORMAT_CHAR + "8";
+	public static final String BLUE = FORMAT_CHAR + "9";
+	public static final String GREEN = FORMAT_CHAR + "a";
+	public static final String CYAN = FORMAT_CHAR + "b";
+	public static final String RED = FORMAT_CHAR + "c";
+	public static final String PINK = FORMAT_CHAR + "d";
+	public static final String YELLOW = FORMAT_CHAR + "e";
+	public static final String WHITE = FORMAT_CHAR + "f";
+
+	/**
+	 * Chat formatting
+	 */
+	public static final String MAGIC = FORMAT_CHAR + "k";
+	public static final String BOLD = FORMAT_CHAR + "l";
+	public static final String STRIKETHROUGH = FORMAT_CHAR + "m";
+	public static final String UNDERLINE = FORMAT_CHAR + "n";
+	public static final String ITALIC = FORMAT_CHAR + "o";
+
+	/**
+	 * Reset sequence
+	 */
+	public static final String RESET = FORMAT_CHAR + "r";
+
+	/**
+	 * String containing all valid formatting codes
+	 */
+	public static final String valid = "0123456789AaBbCcDdEeFfKkLlMmNnOoPpRr";
+
+	/**
+	 * Replaces color codes (prefixed with ampersands) with the Minecraft color
+	 * escape sequences
+	 *
+	 * @param message Message to format
+	 * @return Formatted message
+	 */
+	public static String format(String message) {
+		return format(message, '&');
+	}
+
+	/**
+	 * Replaces color codes (with specified prefix) with the Minecraft color
+	 * escape sequences
+	 *
+	 * @param message Message to format
+	 * @param escape Character used to escape color codes
+	 * @return Formatted message
+	 */
+	public static String format(String message, char escape) {
+		if (message.isEmpty()) return "";
+
+		char[] s = message.toCharArray();
+		int len = s.length;
+
+		for (int i = 0; i < len; i++) {
+			char check = s[i];
+
+			// Check if this is the escape character
+			if (check != escape) continue;
+
+			// Make sure another character exists
+			if ((i + 1) == len) break;
+
+			char next = s[i + 1];
+
+			// Check if it's a valid color character
+			if (valid.indexOf(next) > -1) {
+				s[i] = FORMAT_CHAR;
+				i++;
+				// Uppercase causes issues
+				s[i] = Character.toLowerCase(s[i]);
+			}
+		}
+
+		return new String(s);
+	}
+
+}
---
+++ api/src/test/java/org/cubeville/util/ColorTest.java
@@ -0,0 +1,18 @@
+package org.cubeville.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ColorTest {
+
+	@Test
+	public void formatColorCodes() {
+		String orig = "&0&1&2&3&4&5&6&7&8&9&A&a&B&b&C&c&D&d&E&e&F&f&G&g";
+		String expected = Chat.BLACK + Chat.DARK_BLUE + Chat.DARK_GREEN + Chat.DARK_CYAN + Chat.DARK_RED + Chat.PURPLE + Chat.GOLD + Chat.GRAY + Chat.DARK_GRAY + Chat.BLUE + Chat.GREEN + Chat.GREEN + Chat.CYAN + Chat.CYAN + Chat.RED + Chat.RED + Chat.PINK + Chat.PINK + Chat.YELLOW + Chat.YELLOW + Chat.WHITE + Chat.WHITE + "&G&g";
+		String format = Chat.format(orig);
+
+		assertEquals(expected, format);
+	}
+
+}
