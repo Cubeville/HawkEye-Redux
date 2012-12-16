71519c5526524bed932cbb2cb1224df911f7de5b
GoalieGuy6 <goalieguy6@gmail.com>
2012-12-16 12:04:05 -0500

    Javadocs

--- api/src/main/java/org/cubeville/hawkeye/command/Command.java
+++ api/src/main/java/org/cubeville/hawkeye/command/Command.java
@@ -1,3 +1,21 @@
+/*
+ * HawkEye
+ * Copyright (C) 2012 Cubeville <http://www.cubeville.org> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cubeville.hawkeye.command;
 
 import java.lang.annotation.ElementType;
@@ -28,6 +46,11 @@ public @interface Command {
 	 * Command description
 	 */
 	String description() default "";
+
+	/**
+	 * Whether or not this command can only be run by a player
+	 */
+	boolean player() default false;
 
 	/**
 	 * List of flags that can be used with this command
--- api/src/main/java/org/cubeville/hawkeye/command/CommandData.java
+++ api/src/main/java/org/cubeville/hawkeye/command/CommandData.java
@@ -1,3 +1,21 @@
+/*
+ * HawkEye
+ * Copyright (C) 2012 Cubeville <http://www.cubeville.org> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cubeville.hawkeye.command;
 
 import java.util.HashSet;
@@ -41,30 +59,72 @@ public class CommandData {
 		}
 	}
 
+	/**
+	 * Gets the number of arguments that this command data holds
+	 *
+	 * @return Number of arguments (not including flags)
+	 */
 	public int length() {
 		return processedArgs.size();
 	}
 
+	/**
+	 * Gets the command that this object represents
+	 *
+	 * @return Base command
+	 */
 	public String getCommand() {
 		return command;
 	}
 
+	/**
+	 * Gets a command argument as a string
+	 *
+	 * @param index Index of the argument
+	 * @return The argument
+	 */
 	public String getString(int index) {
 		return processedArgs.get(index);
 	}
 
+	/**
+	 * Gets several arguments joined into a full string
+	 *
+	 * @param startIndex Starting index
+	 * @return String of all arguments at and after start index
+	 */
 	public String getFullString(int startIndex) {
 		return StringUtil.buildString(processedArgs, " ", startIndex);
 	}
 
+	/**
+	 * Gets an argument as an integer
+	 *
+	 * @param index Index of the argument
+	 * @return int
+	 * @throws NumberFormatException If the argument is not a number
+	 */
 	public int getInt(int index) throws NumberFormatException {
 		return Integer.parseInt(processedArgs.get(index));
 	}
 
+	/**
+	 * Gets an argument as a double
+	 *
+	 * @param index Index of the argument
+	 * @return double
+	 * @throws NumberFormatException If the argument is not a number
+	 */
 	public double getDouble(int index) throws NumberFormatException {
 		return Double.parseDouble(processedArgs.get(index));
 	}
 
+	/**
+	 * Checks if a flag was defined
+	 *
+	 * @param flag Flag to check for
+	 * @return True if flag was defined, false if not
+	 */
 	public boolean hasFlag(char flag) {
 		return flags.contains(flag);
 	}
