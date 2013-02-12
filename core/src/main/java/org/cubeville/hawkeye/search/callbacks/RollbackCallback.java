95ea9152cf0ade1076928934945fd99d46db256b
GoalieGuy6 <goalieguy6@gmail.com>
2013-02-12 08:36:15 -0500

    Added rollback callback

---
+++ core/src/main/java/org/cubeville/hawkeye/search/callbacks/RollbackCallback.java
@@ -0,0 +1,39 @@
+/*
+ * HawkEye Redux
+ * Copyright (C) 2012-2013 Cubeville <http://www.cubeville.org> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.cubeville.hawkeye.search.callbacks;
+
+import java.util.List;
+
+import org.cubeville.hawkeye.model.Entry;
+import org.cubeville.hawkeye.session.Session;
+
+public class RollbackCallback extends AbstractCallback {
+
+	public RollbackCallback(Session session) {
+		super(session);
+	}
+
+	@Override
+	public void execute(List<Entry> results) {
+		super.execute(results);
+
+		// TODO Rollback stuff
+	}
+
+}
